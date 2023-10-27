10d3248fd11bad42c58df31256c68b59d552cca4
LukasMei <lukasmeissner@web.de>
2023-10-27 12:46:03 +0200

    fertig und abgegeben

--- src/Laboruebung_01.java
+++ src/Laboruebung_01.java
@@ -1,22 +1,19 @@
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 
 public class Laboruebung_01 {
-    /**
-     * @param args
-     * @throws Exception
-     */
-    public static void main(String[] args) throws Exception {
-
-        String local = InetAddress.getLocalHost().toString();
-        String[] arrLocal = local.split("/");
-        System.out.println(arrLocal[0]);
-        System.out.println(arrLocal[1]); 
-          
-            
-      
+    
+     
+    public static void main(String[] args) throws UnknownHostException  {      
         
-            
-        
+                 
+        try {
+            System.out.println(InetAddress.getLocalHost().getCanonicalHostName());
+            System.out.println(InetAddress.getLocalHost().getHostAddress());
+        } catch (UnknownHostException e) {            
+            e.printStackTrace();
+        }
+    
     }
 }
