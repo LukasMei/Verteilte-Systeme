b8e0935146c39b33a0c73472d5c7a735fa05af7d
LukasMei <lukasmeissner@web.de>
2023-12-14 19:03:47 +0100

    different approach

--- src/Laboruebung_04/SimpleClient.java
+++ src/Laboruebung_04/SimpleClient.java
@@ -5,39 +5,37 @@ package Laboruebung_04;
                     Do not import them twice!
                     Additional imports are not necessary and NOT allowed.
 */
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
-import java.nio.charset.StandardCharsets;
+    import java.io.BufferedReader;
+    import java.io.File;
+    import java.io.FileNotFoundException;
+    import java.io.FileReader;
+    import java.io.FileWriter;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.net.InetSocketAddress;
+    import java.nio.ByteBuffer;
+    import java.nio.channels.SocketChannel;
+    import java.nio.charset.StandardCharsets;
 
 // code line number: 280
 public class SimpleClient implements Runnable {
-
+    
 	private Thread thread = null;
 	private String groupName = "Freitag-Teams";
 	private String userName;
 	private String message;
 	private boolean running;
-
-	private static final int BUF_SIZE = 264;
-
+    private static final int BUF_SIZE = 264;
 	private static final String DSLP_EOL = "\r\n";
-	private static final String DSLP_HEADER = "dslp-4.0";
+	private static final String DSLP_HEADER = "dslp-4.0/";
 	private static final String DSLP_GROUP_JOIN = "group join";
-	private static final String DSLP_GROUP_JOIN_NOTIFY = "group notify";
-	private static final String DSLP_GROUP_LEAVE = "group leave";
-	private static final String DSLP_GROUP_NAME = "Freitag-Teams";
 	private static final String DSLP_GROUP_JOIN_ACK = "group join ack";
+	private static final String DSLP_GROUP_NOTIFY = "group notify";
+	private static final String DSLP_GROUP_LEAVE = "group leave";
+	private static final String DSLP_GROUP_LEAVE_ACK = "group leave ack";
+	private static final String DSLP_GROUP_NAME = "Freitag-Teams";
 	private static final String DSLP_BODY = "dslp-body";
 
-	
 
 	public SimpleClient(String name, String message) {
 		this.thread = new Thread(this, name);
@@ -52,7 +50,7 @@ public class SimpleClient implements Runnable {
 
 	public void stop() {
 		running = false;
-	}
+	}		
 
 	@Override
 	public void run() {
@@ -60,66 +58,113 @@ public class SimpleClient implements Runnable {
 		int port = 31000;
 		SocketChannel sc = null;
 		try {
-			sc = SocketChannel.open();
-			InetSocketAddress address = new InetSocketAddress(host, port);
-			sc.connect(address);
-			System.out.println("Verbindung erfolgreich");
+            sc = SocketChannel.open();
+			InetSocketAddress adresse = new InetSocketAddress(host, port);
+		    sc.connect(adresse);
+			System.out.println(adresse);
+			System.out.println(" Verbindung geschafft");
 			running = true;
-
+			write(sc, (DSLP_HEADER + DSLP_GROUP_JOIN + DSLP_EOL + DSLP_GROUP_NAME + DSLP_EOL + DSLP_BODY + DSLP_EOL));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		if (sc != null) {
-			while (running) {
-				try {
-					write(sc, (DSLP_HEADER + DSLP_GROUP_JOIN + DSLP_EOL + DSLP_GROUP_NAME + DSLP_EOL + DSLP_BODY));					
-					String response = read(sc);
-					String[] split_line_break = response.split("\r}n");
-					for (String zeile : split_line_break) {
-						System.out.println(zeile);
+		if(sc != null) {
+			while(running) {
+		        try {
+                    String resp = read(sc);
+					String[] splitArray = resp.split("\r\n");           //Trennung der Zeilen mit \r\n
+					for(String zeile : splitArray){
+						if(zeile.equals("dslp-4.0/group join ack") || zeile.equals("Freitag-Teams") || zeile.equals("dslp-body")){
+							System.out.println(zeile);
+						}else{
+							System.out.println("Falsche Ausgabe vom Server !");
+						}
 					}
-					Thread.sleep(1000);
 					running = false;
-					sc.close();
-
-				} catch (Exception e) {
+					break;
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-
+		        // write(..)
+		        // ...
 			}
+		}
 
+		//zweite Nachricht
+		try {
+			running = true;
+			write(sc, (DSLP_HEADER + DSLP_GROUP_NOTIFY + DSLP_EOL + this.userName + DSLP_EOL + DSLP_GROUP_NAME + DSLP_EOL + "1" + DSLP_EOL + DSLP_BODY + DSLP_EOL + this.message + DSLP_EOL));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if(sc != null) {
+			while(running) {
+		        try {
+                    String resp = read(sc);
+					String[] splitArray = resp.split("\r\n");           //Trennung der Zeilen mit \r\n
+					for(String zeile : splitArray){
+						System.out.println(zeile);
+					}
+					running = false;
+					break;
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        // write(..)
+		        // ...
+			}
+		}
+
+		//dritte Nachricht
+		try {
+			running = true;
+			write(sc, (DSLP_HEADER + DSLP_GROUP_LEAVE + DSLP_EOL + DSLP_GROUP_NAME + DSLP_EOL + DSLP_BODY + DSLP_EOL));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if(sc != null) {
+			while(running) {
+		        try {
+                    String resp = read(sc);
+					String[] splitArray = resp.split("\r\n");           //Trennung der Zeilen mit \r\n
+					for(String zeile : splitArray){
+						System.out.println(zeile);
+					}
+					running = false;
+					sc.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        // write(..)
+		        // ...
+			}
 		}
 
 	}
 
-	/*
-	 * Initialisierung eines ByteBuffer Objektes. Der Socketchannel liest den Buffer
-	 * ein.
-	 * Umwandlung der Nachricht in einen String.
-	 */
 	private String read(SocketChannel sc) throws IOException {
-
-		ByteBuffer buffer = ByteBuffer.allocate(BUF_SIZE);
-		sc.read(buffer);
-		String answer = new String(buffer.array(), StandardCharsets.UTF_8);
-
+	    ByteBuffer buf = ByteBuffer.allocate(BUF_SIZE);				//neuer Buffer
+		sc.read(buf);												//socketchannel liest in den buffer rein
+		
+		String answer  = new String(buf.array(), StandardCharsets.UTF_8);   //Umwandlung in String mit UTF_8
 		return answer;
+
 	}
 
-	/*
-	 * Wir übergebn die Nachricht an ein byte Array.
-	 * Initialisieren einen neuen ByteBuffer mit der gräße Buff_Size.
-	 * Das byte Array wird vom Byte Buffer gewrappt und dann vom socketchanlle
-	 * geschrieben.
-	 * 
-	 */
 	private void write(SocketChannel sc, String msg) throws IOException {
-		byte[] data = msg.getBytes(StandardCharsets.UTF_8);
+		byte[] dataBytes = msg.getBytes(StandardCharsets.UTF_8);               //Nachricht wird als Byte Array übergeben und gleichzeitig in UTF8 umgewandelt 
 
-		ByteBuffer buffer = ByteBuffer.allocate(BUF_SIZE);
-		buffer = ByteBuffer.wrap(data);
-
-		sc.write(buffer);
+		ByteBuffer buf = ByteBuffer.allocate(BUF_SIZE);							//neuer Buffer mit größe BUF_SIZE
+		buf = ByteBuffer.wrap(dataBytes);   									//byte array wird gewrappt und dem Buffer zugewiesen
+		sc.write(buf);															// socketchannel schreibt buffer
 	}
+	
 
 }
